c18fabf18f714f37da466358714ed79641a99943
Cleiton Paiva <cleibp@gmail.com>
2023-10-10 08:34:50 -0300

    feat: add try and enum

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
-int soma(int num1, int num2);
+
+enum class Cor { Vermelho, Verde, Azul, Amarelo, Laranja };
+
+int soma(int a, int b) {
+    return a + b;
+}
 int main() {
 
   char nome[] = "Cleiton";
   int idade = 20;
-  // char sexo[1] = "M";
+  char sexo = 'M';
   double peso = 70.5;
-  // bool ativo = true;
+  bool ativo = true;
 
-  int val1, val2, soma, subtracao, multiplicacao, divisao, modulo;
+  int val1, val2, adicao, subtracao, multiplicacao, divisao, modulo;
 
   int idadeTernario;
 
@@ -25,6 +30,10 @@ int main() {
   int m = 0;
   int n = 0;
 
+  int *ponteiro; // Declaração de um ponteiro para um inteiro
+
+  int numero1, numero2, res;
+
 // CONSTANTE
 #define PI 3.14159265
 
@@ -34,16 +43,17 @@ int main() {
     de varias linhas */
 
   // ESCREVER NA TELA
-  std::cout << "#### Escrever na tela #### \n";
+  std::cout << "#### ESCREVER NA TELA #### \n";
   std::cout << "Olá Mundo \n";
   std::cout << "\n";
 
   // VARIAVEIS
-  std::cout << "### VARIAVEIS ###\n";
+  std::cout << "### VARIAVEIS E TIPOS BÁSICOS ###\n";
   std::cout << "Nome: " << nome << "\n";
   std::cout << "Idade: " << idade << "\n";
-  // std::cout << "Sexo: " << sexo << "\n";
+  std::cout << "Sexo: " << sexo << "\n";
   std::cout << "Peso: " << peso << "\n";
+  std::cout << "Ativo: " << ativo << "\n";
   std::cout << "\n";
 
   // CONSTANTE
@@ -59,12 +69,12 @@ int main() {
   std::cout << "Informe o valor 2: ";
   std::cin >> val2;
 
-  soma = val1 + val2; // Pode usar: (+, -, *, /, %)
+  adicao = val1 + val2; // Pode usar: (+, -, *, /, %)
   subtracao = val1 - val2;
   multiplicacao = val1 * val2;
   divisao = val1 / val2;
   modulo = val1 % val2;
-  std::cout << "Soma: " << soma << "\n";
+  std::cout << "Soma: " << adicao << "\n";
   std::cout << "Subtração: " << subtracao << "\n";
   std::cout << "Multiplicação: " << multiplicacao << "\n";
   std::cout << "Divisão: " << divisao << "\n";
@@ -74,8 +84,9 @@ int main() {
   // TERNARIO
   std::cout << "### TERNARIO ###\n";
   std::cout << "Digite um número: ";
-  std::cin  >>  idadeTernario;
-  idadeTernario >= 18 ? std::cout << "Maior de idade \n" : std::cout << "Menor de idade\n";
+  std::cin >> idadeTernario;
+  idadeTernario >= 18 ? std::cout << "Maior de idade \n"
+                      : std::cout << "Menor de idade\n";
   std::cout << "\n";
 
   // IF ELSE IF ELSE
@@ -160,19 +171,109 @@ int main() {
   }
   std::cout << "\n";
 
+      // ARRAY
+    std::cout << "### ARRAY ###" << std::endl;
+    int numbers[] = {10, 20, 30, 40};
+
+    for (int i = 0; i < 4; i++) {
+        std::cout << numbers[i] << std::endl;
+    }
+    std::cout << std::endl;
+
+    // MATRIZ
+    std::cout << "### MATRIZ ###" << std::endl;
+    int matriz[3][3]; // Declaração de uma matriz 3x3 de inteiros
+
+    // Inicialização da matriz
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            matriz[i][j] = i * 3 + j + 1;
+        }
+    }
+
+    // Acesso aos elementos da matriz
+    std::cout << "Elementos da matriz:" << std::endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            std::cout << matriz[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+
   // FUNCAO
   std::cout << "### FUNCAO ###\n";
-  std::cout<< "Digite o valor 1 \n";
-  std::cin>> a;
+  std::cout<< "Digite o valor 1: ";
+  std::cin>> m;
 
-  std::cout<< "Digite o valor 2 \n";
-  std::cin>> b;
+  std::cout<< "Digite o valor 2: ";
+  std::cin>> n;
 
-  // std::cout<< "Soma \n" << soma(a, b);
+  int resultado = soma(m, n);
+  std::cout << "A soma de " << m << " e " << n << " é igual a " << resultado;
+  std::cout << "\n";
 
   std::cout << "\n";
-}
+  
+  // PROCEDURE
+  std::cout << "### PROCEDURE ###\n";
+  std::cout << "Não tem PROCEDURE \n";
+  std::cout << "\n";
 
-int soma( int num1, int num2) {
-   return num1 + num2;
+  std::cout << "\n";
+
+  // PONTEIRO
+  std::cout << "### PONTEIRO ###\n";
+  // Alocação de memória dinamicamente para um inteiro
+  ponteiro = new int;
+
+  *ponteiro = 42; // Atribui um valor à variável apontada
+
+  std::cout << "Valor da variável apontada: " << *ponteiro << std::endl;
+  
+  // Liberação da memória alocada
+  delete ponteiro;
+  std::cout << "\n";
+  
+  // TRY
+  std::cout << "### TRY ###\n";
+  std::cout << "Digite dois números para a divisão:" << std::endl;
+  std::cin >> numero1 >> numero2;
+    
+  try {
+      if (numero2 == 0) {
+          throw "Divisão por zero não é permitida!";
+      }
+      res = numero1 / numero2;
+      std::cout << "Resultado da divisão: " << res << std::endl;
+  } catch (const char* mensagem) {
+      std::cout << "Ocorreu uma exceção: " << mensagem << std::endl;
+  }
+  std::cout << "\n";
+
+
+  // ENUM
+  std::cout << "### ENUM ###\n";
+  Cor minhaCor = Cor::Azul;
+
+  switch (minhaCor) {
+      case Cor::Vermelho:
+          std::cout << "Minha cor favorita é vermelho." << std::endl;
+          break;
+      case Cor::Verde:
+          std::cout << "Minha cor favorita é verde." << std::endl;
+          break;
+      case Cor::Azul:
+          std::cout << "Minha cor favorita é azul." << std::endl;
+          break;
+      case Cor::Amarelo:
+          std::cout << "Minha cor favorita é amarelo." << std::endl;
+          break;
+      case Cor::Laranja:
+          std::cout << "Minha cor favorita é laranja." << std::endl;
+          break;
+      default:
+          std::cout << "Eu não tenho uma cor favorita." << std::endl;
+  }
+  
 }
